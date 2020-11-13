856e8c1b2ade9850eefd5f618c3c11cfeb4c94c3
Ge (Melody) Zhang <59809428+MelodyZhangHelloWorld@users.noreply.github.com>
2020-11-13 12:52:30 -0700

    firebaseConfig&firebaseInit-setUp

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,12 @@
+export default{
+
+    apiKey: "AIzaSyDxSI-ZW_z23D0wnETS0TBDmWXEbdoMH54",
+  authDomain: "simpletodoappvue.firebaseapp.com",
+  databaseURL: "https://simpletodoappvue.firebaseio.com",
+  projectId: "simpletodoappvue",
+  storageBucket: "simpletodoappvue.appspot.com",
+  messagingSenderId: "877677181054",
+  appId: "1:877677181054:web:b9aafe9636c35f1ab5c02f",
+  measurementId: "G-K6GB6J9VLK"
+
+}
