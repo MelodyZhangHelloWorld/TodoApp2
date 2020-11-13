1dfc927da714a6238f98fbbb8ffaaec9f9c042ac
Ge (Melody) Zhang <59809428+MelodyZhangHelloWorld@users.noreply.github.com>
2020-11-12 17:55:23 -0700

    simpleDisplay-beforeAxios

--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -2,17 +2,44 @@
 <div class="container">
   <h1> TodoList</h1>
 
+  <div class="todoList">
+    <div v-for="todo in allTodos" 
+    :key="todo.id" class="todo"> 
+    {{ todo.title}}
+    </div>
+
+  </div>
+
+
 </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 
 export default {
-    name: "todoList"
+    name: "todoList",
+
+    computed: mapGetters(['allTodos']) //** why []??
 
 }
 </script>
 
-<style>
+<style scoped>
+
+.todoList {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
 
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import todoList from "./modules/todoList"; //***
+
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {
+    todoList
+  }
 });
